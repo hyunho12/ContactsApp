02575bc6037fc859fae8d2f87b4e6910ad3d8a9c
김현호 <hyundho12@gmail.com>
2023-08-14 10:34:29 +0900

    apidbcontext 생성

---
+++ ConactsWebApi/Data/ApiDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConactsWebApi.Data
+{
+    public class ApiDbContext : DbContext 
+    {
+
+        public ApiDbContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
